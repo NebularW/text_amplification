5d2bb754a8566160d6baeda829834451501a3484
RuiChen Xue <91529148+RashawnXue@users.noreply.github.com>
2022-06-29 18:46:09 +0800

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -1,22 +1,42 @@
 import requests
+import time
+import re
 from bs4 import BeautifulSoup
+
+global count
+count = 0
+
+def split_text(text):
+    text = re.sub('([。！？\?])([^”’])', r"\1\n\2", text) 
+    text = re.sub('([。！？\?][”’])([^，。！？\?])', r'\1\n\2', text)
+    text = text.rstrip() 
+    return text.split("\n")
 
 
 def get_text(url):
-    src = requests.get(url).content
-    # print(page_source)
-    bs_src = BeautifulSoup(src)
-    report_text = bs_src.find_all('p')
-    text = ''
-    for p in report_text:
-        text += p.get_text()
-        text += '\n'
-    return text
-
+    global count
+    try:
+        src = requests.get(url).content
+        bs_src = BeautifulSoup(src)
+        nodes = bs_src.find_all('p')
+        text = ''
+        for p in nodes:
+            text = p.get_text()
+            words = split_text(text)
+            for word in words:
+                if word.strip() == '':
+                    continue
+                save(f'..\\crawler\\texts\\content{count}.txt', word)
+                print(count)
+                count+=1
+                if count >= 200:
+                    return
+    except requests.RequestException:
+        return None
 
 def get_urls(root_url):
-    src = requests.get(root_url).content
-    bs_src = BeautifulSoup(src)
+    src = requests.get(root_url , verify=False).content
+    bs_src = BeautifulSoup(src, "lxml")
     hrefs = bs_src.find_all('a')
     links = []
     for a in hrefs:
@@ -24,27 +44,20 @@ def get_urls(root_url):
             if 'http' in a.get('href'):
                 link = a.get('href')
             else:
-                link = root_url + '/' + a.get('href')
-            if link.strip():
+                link = root_url + a.get('href')
+            if link.strip()!="":
                 links.append(link)
     return links
-
 
 def save(filename, contents):
     fh = open(filename, 'w', encoding='utf-8')
     fh.write(contents)
     fh.close()
 
-
-def Crawler():
-    url = 'https://www.zj.gov.cn/'
-    links = get_urls(url)
-    i = 0
+if __name__ == '__main__':
+    root_url = 'https://www.zj.gov.cn/'
+    links = get_urls(root_url)
     for link in links:
-        text = get_text(link)
-        if text:
-            save(f'.\\text\\content{i}.txt', text)
-            i += 1
-            print(i)
-        if i >= 100:
+        get_text(link)
+        if count >= 200:
             break
